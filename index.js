8ab9291496576a9a96cf4a035abf6a25439f3d2a
parthasaikrishna <parthasaikrishna.m@gmail.com>
2024-11-07 07:15:50 +0530

    done

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ app.get('/read',async (req,res)=>{
     res.render("index",{listOfUsers:allUsers});
 })
 
-app.get('/delete/:id',async (req,res)=>{
-   let users = await userModel.findOneAndDelete({_id:req.params.id});
-    res.redirect("/read");
-})
-
 app.post('/create',async (req,res)=>{
     let {name,email,image} = req.body;
    let createdUser= await userModel.create({
@@ -31,5 +26,22 @@ app.post('/create',async (req,res)=>{
     })
     res.redirect("/read");
 })
+
+app.get('/delete/:id',async (req,res)=>{
+   let users = await userModel.findOneAndDelete({_id:req.params.id});
+    res.redirect("/read");
+})
+
+app.get("/edit/:userid",async (req,res)=>{
+   let user= await userModel.findOne({_id:req.params.userid});
+       res.render("edit",{user});
+})
+
+app.post('/update/:userid',async (req,res)=>{
+    let {image,email,name} = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({_id:req.params.userid},{image,name,email},{new:true});
+    res.redirect('/read'); 
+})
+
 app.listen(3001);
 
